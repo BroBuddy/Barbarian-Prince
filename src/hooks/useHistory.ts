cf86a96ff5f81c0cfceef3084f608eae5b8ede87
Buddy Stone <buddy.cgn@gmail.com>
2026-04-02 01:16:31 +0200

    fixed history area

--- src/hooks/useHistory.ts
+++ src/hooks/useHistory.ts
@@ -1,10 +1,12 @@
+import type { HistoryItem } from "@/pages/History";
+
 export const useHistory = () => {
   const storage: string = "barbarian-prince";
 
-  const addToHistory = (id: string, title: string) => {
+  const addToHistory = (tag: string, title: string) => {
     const history = JSON.parse(localStorage.getItem(storage) || "[]");
-    const filtered = history.filter((item: any) => item.id !== id);
-    const updated = [{ id, title }, ...filtered].slice(0, 10);
+    const filtered = history.filter((item: HistoryItem) => item.tag !== tag);
+    const updated = [{ tag, title }, ...filtered].slice(0, 10);
     localStorage.setItem(storage, JSON.stringify(updated));
   };
 
--- src/pages/History.tsx
+++ src/pages/History.tsx
@@ -1,6 +1,11 @@
 import { Link } from "react-router-dom";
 import Card from "../components/Card";
 
+export type HistoryItem = {
+  tag: string;
+  title: string;
+};
+
 function History() {
   const history = JSON.parse(localStorage.getItem("barbarian-prince") || "[]");
 
@@ -9,9 +14,9 @@ function History() {
       <Card title="History">
         {history.length === 0 && <p>No history yet.</p>}
 
-        {history.map((item: { id: string; title: string }, index: number) => (
-          <Link to={`/event/${item.id}`} key={index} className="flex flex-row">
-            <span className="m-2">{item.id}</span>
+        {history.map((item: HistoryItem, index: number) => (
+          <Link to={`/event/${item.tag}`} key={index} className="flex flex-row">
+            <span className="m-2">{item.tag}</span>
             <span className="my-2 text-black">{item.title}</span>
           </Link>
         ))}
